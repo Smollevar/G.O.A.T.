c6dfe6c02fbff55b56da3317f47325fa7089f167
Dmitry Kravtsov <e4ahl1bhyr@gmail.com>
2021-09-04 15:07:38 +0700

    add 17 day

---
+++ src/main/java/day17/ChessBoard.java
@@ -0,0 +1,18 @@
+package day17;
+
+public class ChessBoard {
+    private ChessPiece[][] chessArray;
+
+    public ChessBoard(ChessPiece chessArray[][]) {
+        this.chessArray = chessArray;
+    }
+
+    public void print() {
+        for (int i = 0; i < chessArray.length; i++) {
+            for (int j = 0; j < chessArray[i].length; j++) {
+                System.out.print(chessArray[i][j].getColorPiece());
+            }
+            System.out.println();
+        }
+    }
+}
--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,19 @@
 package day17;
 
+import java.util.*;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        List<ChessPiece> pieces = new ArrayList<>();
+        pieces.add(ChessPiece.PAWN_WHITE);
+        pieces.add(ChessPiece.PAWN_WHITE);
+        pieces.add(ChessPiece.PAWN_WHITE);
+        pieces.add(ChessPiece.PAWN_WHITE);
+        pieces.add(ChessPiece.ROOK_BLACK);
+        pieces.add(ChessPiece.ROOK_BLACK);
+        pieces.add(ChessPiece.ROOK_BLACK);
+        pieces.add(ChessPiece.ROOK_BLACK);
+        for (ChessPiece piece : pieces)
+            System.out.print(piece.getColorPiece()+" ");
     }
 }
